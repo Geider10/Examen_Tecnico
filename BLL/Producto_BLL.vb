477599bab7bfb181088832a0f6f716d9b1b3eff9
Geider Frias <geiderde20@gmail.com>
2025-02-13 23:30:07 -0300

    create product business-logic and his functions

---
+++ BLL/Producto_BLL.vb
@@ -0,0 +1,24 @@
+﻿Imports DAL
+Imports Entity
+
+Public Class Producto_BLL
+    Public Function GetProductos() As DataTable
+        Dim productoDal As Producto_DAL = New Producto_DAL()
+        Return productoDal.GetProductos()
+    End Function
+
+    Public Sub Add(producto As Producto)
+        Dim productoDAL As Producto_DAL = New Producto_DAL()
+        productoDAL.Add(producto)
+    End Sub
+
+    Public Sub Update(producto As Producto, id As Integer)
+        Dim productoDAL As Producto_DAL = New Producto_DAL()
+        productoDAL.Update(producto, id)
+    End Sub
+
+    Public Sub Delete(id As Integer)
+        Dim productoDAL As Producto_DAL = New Producto_DAL()
+        productoDAL.Delete(id)
+    End Sub
+End Class
